7f59401e6c185ad8f9c273723169d8c6e87eae4e
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-04-26 10:59:41 +0900

    🔥 不要になったutilを一旦削除

--- apps/web/src/utils/cookie.ts
+++
@@ -1,8 +0,0 @@
-export const readTokenFromCookie = (cookieName = 'accessTokenFromGoBackend'): string | null => {
-  const match = document.cookie.match(new RegExp(`(?:^|; )${cookieName}=([^;]*)`));
-  return match ? decodeURIComponent(match[1]) : null;
-};
-
-export const deleteCookie = (cookieName: string) => {
-  document.cookie = `${cookieName}=; Max-Age=0; path=/`;
-};
