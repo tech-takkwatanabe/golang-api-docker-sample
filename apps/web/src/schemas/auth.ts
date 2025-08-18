cd747f587786a39d7608d911497a587f8c2a5b60
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-08-18 22:46:18 +0900

    🎨 Zodの非推奨の書き方修正/マイナーバージョン更新

--- apps/web/src/schemas/auth.ts
+++ apps/web/src/schemas/auth.ts
@@ -2,17 +2,17 @@ import { z } from 'zod';
 
 export const nameSchema = z
   .string()
-  .min(2, '名前を入力してください')
-  .max(100, '100文字以内で入力してください');
+  .min(2, { message: '名前を入力してください' })
+  .max(100, { message: '100文字以内で入力してください' });
 export const emailSchema = z
   .string()
-  .email('有効なメールアドレスを入力してください')
-  .max(320, '入力文字数が多すぎます');
+  .email({ message: '有効なメールアドレスを入力してください' })
+  .max(320, { message: '入力文字数が多すぎます' });
 export const registPasswordSchema = z
   .string()
-  .min(6, 'パスワードは6文字以上である必要があります')
-  .max(200, '入力文字数が多すぎます');
+  .min(6, { message: 'パスワードは6文字以上である必要があります' })
+  .max(200, { message: '入力文字数が多すぎます' });
 export const loginPasswordSchema = z
   .string()
-  .min(1, 'パスワードを入力してください')
-  .max(200, '入力文字数が多すぎます');
+  .min(1, { message: 'パスワードを入力してください' })
+  .max(200, { message: '入力文字数が多すぎます' });
